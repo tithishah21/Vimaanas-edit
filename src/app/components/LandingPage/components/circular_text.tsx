ed68a31c927f1d62037445f0fbfaca40b0453537
Tithi Shah <shahtithi21@gmail.com>
2025-03-13 22:02:34 +0530

    Fix:- Responsive - VIT Logo animation

--- src/app/components/LandingPage/components/circular_text.tsx
+++ src/app/components/LandingPage/components/circular_text.tsx
@@ -108,7 +108,7 @@ const CircularText: React.FC<CircularTextProps> = ({
         return (
           <span
             key={i}
-            className="absolute inline-block inset-0 text-2xl transition-all duration-500 ease-[cubic-bezier(0,0,0,1)]"
+            className="absolute inline-block inset-0 text-[9.5px] md:text-base lg:text-2xl transition-all duration-500 ease-[cubic-bezier(0,0,0,1)]"
             style={{ transform, WebkitTransform: transform }}
           >
             {letter}
--- src/app/components/LandingPage/components/vitlogo.tsx
+++ src/app/components/LandingPage/components/vitlogo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const VitLogo = () => {
   return (
-    <div className="relative w-[30vw] h-[30vw] sm:w-[28vw] sm:h-[28vw] md:w-[26vw] md:h-[26vw] lg:w-[22vw] lg:h-[22vw] max-w-[300px] max-h-[300px] flex items-center justify-center">
+    <div className="relative w-[22vw] h-[22vw] sm:w-[28vw] sm:h-[28vw] md:w-[22vw] md:h-[22vw] lg:w-[22vw] lg:h-[22vw] max-w-[300px] max-h-[300px] flex items-center justify-center">
         
       <Image
         src="/assets/ui components/redellipse.png"
@@ -24,13 +24,22 @@ const VitLogo = () => {
           />
         </div>
 
-
-        <div className="lg:hidden">
+        <div className="hidden md:block lg:hidden">
           <CircularText
-            text="VIT • VELLORE • "
+            text="VELLORE INSTITUTE OF TECHNOLOGY • "
             onHover="speedUp"
-            spinDuration={15}
-            className="absolute w-full h-full text-xs font-semibold tracking-tighter leading-none"
+            spinDuration={20}
+            className="absolute w-full h-full text-xs font-semibold tracking-wider leading-none"
+          />
+        </div>
+
+
+        <div className="lg:hidden md:hidden">
+          <CircularText
+            text="VELLORE INSTITUTE OF TECHNOLOGY • "
+            onHover="speedUp"
+            spinDuration={25}
+            className="absolute w-full h-full text-[1px] xs:text-[10px] sm:text-xs font-semibold tracking-tighter leading-3"
           />
         </div>
       </div>
@@ -41,7 +50,7 @@ const VitLogo = () => {
         alt="VIT Logo"
         width={400}
         height={400}
-        className="relative w-[16vw] h-[20vw] sm:w-[14vw] sm:h-[18vw] md:w-[16vw] md:h-[20vw] lg:w-[17vw] lg:h-[23vw] max-w-[200px] max-h-[220px]"
+        className="relative w-[12vw] h-[16vw] sm:w-[14vw] sm:h-[18vw] md:w-[14vw] md:h-[18vw] lg:w-[17vw] lg:h-[23vw] max-w-[200px] max-h-[230px]"
       />
     </div>
   );
